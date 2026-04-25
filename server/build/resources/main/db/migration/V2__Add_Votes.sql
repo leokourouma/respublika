868e3af35dc032dd07dbdb4679a0a37b68bc96c1
leo <leo.kourouma@goedert.lu>
2026-04-25 20:02:26 +0200

    chore: ignore gradle build artifacts

--- server/build/resources/main/db/migration/V2__Add_Votes.sql
+++
@@ -1,9 +0,0 @@
--- server/src/main/resources/db/migration/V2__Add_Votes.sql
-
-CREATE TABLE IF NOT EXISTS votes_deputes (
-    id SERIAL PRIMARY KEY,
-    scrutin_id VARCHAR(50) REFERENCES scrutins(uid),
-    depute_id VARCHAR(20) REFERENCES deputes(id_an),
-    position VARCHAR(15) NOT NULL, -- POUR, CONTRE, ABSTENTION
-    UNIQUE(scrutin_id, depute_id) -- Un député ne vote qu'une fois par scrutin
-);
